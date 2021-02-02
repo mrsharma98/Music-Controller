a4dbf363a5a202cbea2135e7dc00857b38bc5723
Manish Sharma <mrsharma98@gmail.com>
2021-02-02 10:32:12 +0530

    added particular room pages

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-ro
 
 import RoomJoinPage from './RoomJoinPage'
 import CreateRoomPage from './CreateRoomPage'
+import Room from './Room'
 
 export default class HomePage extends Component {
     constructor(props) {
@@ -16,6 +17,7 @@ export default class HomePage extends Component {
                     <Route exact path="/" ><p>This is the Home Page</p></Route>
                     <Route path="/join" component={RoomJoinPage} />
                     <Route path="/create" component={CreateRoomPage} />
+                    <Route path="/room/:roomCode" component={Room} />
                 </Switch>
             </Router>
         )
